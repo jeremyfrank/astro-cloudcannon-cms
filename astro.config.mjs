523dcac98879f2e839b8d2836febd39b84cfa399
Jeremy Frank <jeremyjfrank@gmail.com>
2023-08-25 13:52:49 -0400

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import bookshop from '@bookshop/astro-bookshop'
-
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gnarly-briars.cloudvent.net',
-  integrations: [bookshop(), mdx(), sitemap()],
+  integrations: [bookshop(), mdx(), sitemap(), tailwind()],
 })
