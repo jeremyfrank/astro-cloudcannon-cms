eb27e90714598a56a58c2043a3f99a897914d456
Jeremy Frank <jeremyjfrank@gmail.com>
2023-08-25 11:25:06 -0400

    Add bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import bookshop from '@bookshop/astro-bookshop'
 
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gnarly-briars.cloudvent.net',
-  integrations: [mdx(), sitemap()],
+  integrations: [bookshop(), mdx(), sitemap()],
 })
